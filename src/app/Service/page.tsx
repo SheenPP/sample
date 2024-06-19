5f5352be9d2a309335312709e5eae3a58325b2ec
SheenPP <sheennicholepagayon101219@gmail.com>
2024-06-19 11:18:45 +0800

    modified:   src/app/Service/page.tsx 	modified:   src/app/components/Navbr.tsx

--- src/app/Service/page.tsx
+++ src/app/Service/page.tsx
@@ -7,10 +7,10 @@ const OurService = () => {
             <h2>OUR SERVICE</h2>
             <ul>
                 <li>
-                    Unsere Website wurde geschaffen, um speziell Singles, getrennt lebende oder geschiedene Männer zu helfen, ihren Partner zu finden.
+                    Unsere Website wurde geschaffen, um speziell Singles, getrennt lebende oder geschiedene M&auml;nner zu helfen, ihren Partner zu finden.
                 </li>
                 <li>
-                    Melden Sie sich kostenlos für die ersten 2 Wochen auf unserer Website an. Danach berechnen wir nur 2 Euro pro Monat (für unsere Serviceleistungen wie Übersetzungen, Hilfe beim ersten Treffen, Sprachkurse, Background Checks und mehr…).
+                    Melden Sie sich kostenlos f&uuml;r die ersten 2 Wochen auf unserer Website an. Danach berechnen wir nur 2 Euro pro Monat (f&uuml;r unsere Serviceleistungen wie &Uuml;bersetzungen, Hilfe beim ersten Treffen, Sprachkurse, Background Checks und mehr&hellip;).
                 </li>
                 <li>
                     Ein erstes Treffen organisieren wir gerne hier bei uns auf den Philippinen in unserem Roman-Empire-Resort in einer sicheren Umgebung - mit deutsch-englisch sprechendem Personal.
@@ -20,13 +20,13 @@ const OurService = () => {
                 </li>
             </ul>
 
-            <h3>UND SO GEHTS:</h3>
+            <h3>UND SO GEHT&apos;S:</h3>
             <ol>
                 <li>Melde Dich auf unserer Website an.</li>
-                <li>Schau Dir die Fotogalerie an, sende den Damen, die Dir gefallen, ein "LIKE".</li>
-                <li>Wenn die Damen antworten, kannst Du mit ihnen "kommunizieren - Chatten".</li>
-                <li>Wir haben in den "Chat" eine automatische Übersetzungsfunktion integriert, so dass Du auch mit den Damen schreiben kannst, wenn Du nur geringe oder keine Englisch-Sprachkenntnisse hast.</li>
-                <li>Plane Deinen nächsten Urlaub bei uns und verabrede Dich mit den Damen Deiner Wahl. Hier bei uns trefft ihr euch das erste Mal persönlich, was den hiesigen Damen die nötige Sicherheit gibt und eine ideale Umgebung schafft, um sich näher zu kommen.</li>
+                <li>Schau Dir die Fotogalerie an, sende den Damen, die Dir gefallen, ein &quot;LIKE&quot;.</li>
+                <li>Wenn die Damen antworten, kannst Du mit ihnen &quot;kommunizieren - Chatten&quot;.</li>
+                <li>Wir haben in den &quot;Chat&quot; eine automatische &Uuml;bersetzungsfunktion integriert, so dass Du auch mit den Damen schreiben kannst, wenn Du nur geringe oder keine Englisch-Sprachkenntnisse hast.</li>
+                <li>Plane Deinen n&auml;chsten Urlaub bei uns und verabrede Dich mit den Damen Deiner Wahl. Hier bei uns trefft ihr euch das erste Mal pers&ouml;nlich, was den hiesigen Damen die n&ouml;tige Sicherheit gibt und eine ideale Umgebung schafft, um sich n&auml;her zu kommen.</li>
             </ol>
         </div>
     );
--- src/app/components/Navbr.tsx
+++ src/app/components/Navbr.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { FaBars, FaTimes } from 'react-icons/fa';
 import Image from 'next/image';
 
-
 // Navigation items for the NavBar
 const navItems = [
   { path: "/", name: "Home" },
@@ -16,7 +15,7 @@ const navItems = [
   { path: "/Service", name: "Service" },
 ];
 
- function NavBar() {
+function NavBar() {
   // Get current path or default to "/"
   const pathname = usePathname() || "/";
 
@@ -32,77 +31,77 @@ const navItems = [
   };
 
   return (
-
     <nav className="fixed-top">
-    <div className="flex flex-col md:flex-row justify-between items-center max-w-full sticky top-0 z-60 p-4">
-      {/* Logo Section */}
-      <div className="logo mr-4">
-        <img src="./logo.png" alt="LoveChat Logo" className="h-16 md:h-20" />
-      </div>
+      <div className="flex flex-col md:flex-row justify-between items-center max-w-full sticky top-0 z-60 p-4">
+        {/* Logo Section */}
+        <div className="logo mr-4">
+          <Image src="/logo.png" alt="LoveChat Logo" width={150} height={200} className="h-16 md:h-20" />
+        </div>
 
-      {/* Hamburger Menu for Mobile */}
-      <div className="md:hidden ml-auto" onClick={toggleMenu}>
-        {menuOpen ? <FaTimes className="text-xl text-black" /> : <FaBars className="text-xl text-black" />}
-      </div>
+        {/* Hamburger Menu for Mobile */}
+        <div className="md:hidden ml-auto" onClick={toggleMenu}>
+          {menuOpen ? <FaTimes className="text-xl text-black" /> : <FaBars className="text-xl text-black" />}
+        </div>
 
-      {/* Navigation Links */}
-      <nav className={`NavBar font-sans flex flex-col md:flex-row md:space-x-2 bg-custom-white rounded-2xl shadow-inner absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto transition-all duration-300 ease-in-out ${menuOpen ? "max-h-screen" : "max-h-0 md:max-h-screen"} overflow-hidden md:overflow-visible`}>
-        {navItems.map((item) => {
-          const isActive = item.path === pathname; // Check if the item is the current path
-          const isHovered = item.path === hoveredPath; // Check if the item is being hovered
+        {/* Navigation Links */}
+        <nav className={`NavBar font-sans flex flex-col md:flex-row md:space-x-2 bg-custom-white rounded-2xl shadow-inner absolute md:relative top-16 md:top-0 left-0 w-full md:w-auto transition-all duration-300 ease-in-out ${menuOpen ? "max-h-screen" : "max-h-0 md:max-h-screen"} overflow-hidden md:overflow-visible`}>
+          {navItems.map((item) => {
+            const isActive = item.path === pathname; // Check if the item is the current path
+            const isHovered = item.path === hoveredPath; // Check if the item is being hovered
 
-          return (
-            <Link
-              key={item.path}
-              href={item.path}
-              className={`px-8 py-2 md:py-1 text-sm lg:text-base font-bold rounded-2xl relative no-underline 
+            return (
+              <Link
+                key={item.path}
+                href={item.path}
+                className={`px-8 py-2 md:py-1 text-sm lg:text-base font-bold rounded-2xl relative no-underline 
                 ${isActive ? "bg-custom-red text-white" : "bg-transparent text-black"}
-                hover:bg-gray-200 hover:text-black`}                 
-              data-active={isActive}
-              onMouseEnter={() => setHoveredPath(item.path)}
-              onMouseLeave={() => setHoveredPath(pathname)}
-            >
-              <span>{item.name}</span>
-            </Link>
-          );
-        })}
-      </nav>
+                hover:bg-gray-200 hover:text-black`}
+                data-active={isActive}
+                onMouseEnter={() => setHoveredPath(item.path)}
+                onMouseLeave={() => setHoveredPath(pathname)}
+              >
+                <span>{item.name}</span>
+              </Link>
+            );
+          })}
+        </nav>
 
-      {/* Authentication Links */}
-      <div className="hidden md:flex">
-        <Link
-          href="/signup"
-          className="px-8 py-1 rounded-tl-2xl rounded-bl-2xl text-sm lg:text-base font-bold text-white bg-custom-red no-underline shadow-md"
-        >
-          Signup
-        </Link>
-        <Link
-          href="/login"
-          className="px-8 py-1 rounded-tr-2xl rounded-br-2xl text-sm lg:text-base font-bold text-black bg-gray-100 no-underline shadow-md"
-        >
-          Login
-        </Link>
-      </div>
-
-      {/* Mobile Authentication Links */}
-      {menuOpen && (
-        <div className="flex flex-col md:hidden mt-2">
+        {/* Authentication Links */}
+        <div className="hidden md:flex">
           <Link
             href="/signup"
-            className="px-8 py-2 text-sm text-white font-bold bg-custom-red no-underline shadow-md"
-            >
+            className="px-8 py-1 rounded-tl-2xl rounded-bl-2xl text-sm lg:text-base font-bold text-white bg-custom-red no-underline shadow-md"
+          >
             Signup
           </Link>
           <Link
             href="/login"
-            className="px-8 py-2 text-sm text-black font-bold bg-gray-100 no-underline shadow-md mt-1"
+            className="px-8 py-1 rounded-tr-2xl rounded-br-2xl text-sm lg:text-base font-bold text-black bg-gray-100 no-underline shadow-md"
           >
             Login
           </Link>
         </div>
-      )}
-    </div>
+
+        {/* Mobile Authentication Links */}
+        {menuOpen && (
+          <div className="flex flex-col md:hidden mt-2">
+            <Link
+              href="/signup"
+              className="px-8 py-2 text-sm text-white font-bold bg-custom-red no-underline shadow-md"
+            >
+              Signup
+            </Link>
+            <Link
+              href="/login"
+              className="px-8 py-2 text-sm text-black font-bold bg-gray-100 no-underline shadow-md mt-1"
+            >
+              Login
+            </Link>
+          </div>
+        )}
+      </div>
     </nav>
   );
 }
-export default NavBar
+
+export default NavBar;
