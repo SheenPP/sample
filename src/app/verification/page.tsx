d3d3ed7d244bf6507b113ff1154e919a7a42c126
SheenPP <sheennicholepagayon101219@gmail.com>
2024-07-05 10:30:58 +0800

    modified:   src/app/verification/page.tsx

--- src/app/verification/page.tsx
+++ src/app/verification/page.tsx
@@ -1,92 +1,138 @@
-  "use client";
-  import React, { useState } from 'react';
-  import DockIcon from '@mui/icons-material/Dock';
-  import MailOutlineIcon from '@mui/icons-material/MailOutline';
+"use client";
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { FiMail, FiSmartphone } from 'react-icons/fi';
+import DockIcon from '@mui/icons-material/Dock';
 
-  const Verification = () => {
-    const [mobileInputVisible, setMobileInputVisible] = useState(false);
-    const [emailInputVisible, setEmailInputVisible] = useState(false);
+const Verification = () => {
+  const [mobileInputVisible, setMobileInputVisible] = useState(false);
+  const [emailInputVisible, setEmailInputVisible] = useState(false);
+  const [otpVisible, setOtpVisible] = useState(false);
+  const [verificationMethod, setVerificationMethod] = useState<string>('');
+  const router = useRouter();
 
-    const handleMobileClick = () => {
-      setMobileInputVisible(true);
-      setEmailInputVisible(false);
-    };
-
-    const handleEmailClick = () => {
-      setMobileInputVisible(false);
-      setEmailInputVisible(true);
-    };
-
-    return (
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-cursive font-bold">Welcome</h1>
-        <p className="mt-4 font-mono">Please choose verification method</p>
-        <div className="flex-col gap-4 mt-8 space-y-6 font-mono">
-
-          {/* Mobile Number */}
-          <div className="relative w-full flex items-center">
-            <button 
-              onClick={handleMobileClick} 
-              className={`flex items-center justify-center w-full px-4 py-2 text-lg rounded-full shadow-md shadow-black transition duration-300 ease-in-out ${
-                mobileInputVisible ? 'opacity-0 pointer-events-none' : 'bg-gray-100 text-black hover:bg-white hover:text-custom-red'
-              }`}
-              style={{ transition: 'opacity 0.3s ease-in-out' }}
-            >
-              <DockIcon sx={{ fontSize: 20 }} className="mr-2" />
-              Mobile Number
-            </button>
-            <div className={`absolute inset-0 flex items-center px-4 py-2 text-lg rounded-full shadow-md shadow-black transition-all duration-300 ease-in-out ${
-                mobileInputVisible ? 'opacity-100 bg-custom-red text-white' : 'opacity-0 pointer-events-none'
-              }`}
-              style={{ width: '100%', transition: 'opacity 0.3s ease-in-out' }}
-            >
-              <DockIcon sx={{ fontSize: 20 }} className="mr-2" />
-              <input 
-                type="text" 
-                placeholder="Enter Mobile Number" 
-                className="w-full px-2 py-1 ml-2 bg-white text-black border-none focus:outline-none rounded-full"
-              />
-            </div>
-            {mobileInputVisible && (
-              <button className="ml-4 px-4 py-2 w-56 shadow-black shadow-sm bg-white text-black rounded-full hover:bg-custom-red hover:text-white transition duration-300">
-                Send OTP
-              </button>
-            )}
-          </div>
-
-          {/* Email */}
-          <div className="relative w-full flex items-center">
-            <button 
-              onClick={handleEmailClick} 
-              className={`flex items-center justify-center w-full px-4 py-2 text-lg rounded-full shadow-md shadow-black transition duration-300 ease-in-out ${
-                emailInputVisible ? 'opacity-0 pointer-events-none' : 'bg-gray-100 text-black hover:bg-white hover:text-custom-red'
-              }`}
-              style={{ transition: 'opacity 0.3s ease-in-out' }}
-            >
-              <MailOutlineIcon sx={{ fontSize: 20 }} className="mr-2" />
-              Email
-            </button>
-            <div className={`absolute inset-0 flex items-center px-4 py-2 text-lg rounded-full shadow-md shadow-black transition-all duration-300 ease-in-out ${
-                emailInputVisible ? 'opacity-100 bg-custom-red text-white' : 'opacity-0 pointer-events-none'
-              }`}
-              style={{ width: '100%', transition: 'opacity 0.3s ease-in-out' }}
-            >
-              <MailOutlineIcon sx={{ fontSize: 20 }} className="mr-2" />
-              <input 
-                type="text" 
-                placeholder="Enter Email" 
-                className="w-full px-2 py-1 ml-2 bg-white text-black border-none focus:outline-none rounded-full"
-              />
-            </div>
-            {emailInputVisible && (
-              <button className="ml-4 px-4 py-2 w-56 shadow-black shadow-sm bg-white text-black rounded-full hover:bg-custom-red hover:text-white transition duration-300">
-                Send OTP
-              </button>
-            )}
-          </div>
-        </div>
-      </div>
-    );
+  const handleMobileClick = () => {
+    setMobileInputVisible(true);
+    setEmailInputVisible(false);
   };
 
-  export default Verification;
+  const handleEmailClick = () => {
+    setMobileInputVisible(false);
+    setEmailInputVisible(true);
+  };
+
+  const handleSendOtp = (method: string) => {
+    setOtpVisible(true);
+    setVerificationMethod(method);
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      {!otpVisible ? (
+        <>
+          <h1 className="text-4xl font-bold font-cursive mb-8">Welcome</h1>
+          <p className="mb-4 font-mono">Please choose verification method</p>
+          <div className="flex flex-col gap-6">
+            
+            {/* Mobile Number */}
+            <div className="flex flex-row items-center font-mono">
+              <div className="relative w-full flex items-center mb-4 transition-all duration-500 ease-in-out">
+                {!mobileInputVisible && (
+                  <button 
+                    onClick={handleMobileClick} 
+                    className="flex items-center justify-center w-full px-4 py-2 text-lg rounded-full shadow-md bg-white text-black hover:bg-red-500 hover:text-white transition-all duration-500 ease-in-out"
+                  >
+                    <DockIcon className="mr-2" />
+                    Mobile Number
+                  </button>
+                )}
+                {mobileInputVisible && (
+                  <div className="flex items-center w-full px-4 py-2 text-lg rounded-full shadow-md bg-red-500 text-white transition-all duration-500 ease-in-out">
+                    <DockIcon className="mr-2" />
+                    <input 
+                      type="text" 
+                      placeholder="Enter Mobile Number" 
+                      className="w-full px-2 py-1 ml-2 bg-white text-black border-none focus:outline-none rounded-full"
+                      style={{ transition: 'opacity 0.5s ease-in-out', opacity: mobileInputVisible ? 1 : 0 }}
+                    />
+                  </div>
+                )}
+              </div>
+              {mobileInputVisible && (
+                <button 
+                  onClick={() => handleSendOtp('mobile')}
+                  className="px-4 py-2 w-32 shadow-sm bg-white text-black rounded-full hover:bg-red-500 hover:text-white transition duration-300"
+                >
+                  Send OTP
+                </button>
+              )}
+            </div>
+
+            {/* Email */}
+            <div className="flex flex-row items-center">
+              <div className="relative w-full flex items-center mb-4 transition-all duration-500 ease-in-out">
+                {!emailInputVisible && (
+                  <button 
+                    onClick={handleEmailClick} 
+                    className="flex items-center justify-center w-full px-4 py-2 text-lg rounded-full shadow-md bg-white text-black hover:bg-red-500 hover:text-white transition-all duration-500 ease-in-out"
+                  >
+                    <FiMail className="mr-2" />
+                    Email
+                  </button>
+                )}
+                {emailInputVisible && (
+                  <div className="flex items-center w-full px-4 py-2 text-lg rounded-full shadow-md bg-red-500 text-white transition-all duration-500 ease-in-out">
+                    <FiMail className="mr-2" />
+                    <input 
+                      type="text" 
+                      placeholder="Enter Email" 
+                      className="w-full px-2 py-1 ml-2 bg-white text-black border-none focus:outline-none rounded-full"
+                      style={{ transition: 'opacity 0.5s ease-in-out', opacity: emailInputVisible ? 1 : 0 }}
+                    />
+                  </div>
+                )}
+              </div>
+              {emailInputVisible && (
+                <button 
+                  onClick={() => handleSendOtp('email')}
+                  className="px-4 py-2 w-32 shadow-sm bg-white text-black rounded-full hover:bg-red-500 hover:text-white transition duration-300"
+                >
+                  Send OTP
+                </button>
+              )}
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className="w-80">
+          <div className="flex justify-center mb-6">
+            <div className="text-custom-red p-6">
+              {verificationMethod === 'mobile' ? (
+                <FiSmartphone className="w-32 h-32" />
+              ) : (
+                <FiMail className="w-32 h-32" />
+              )}
+            </div>
+          </div>
+          <h2 className="text-2xl font-bold font-cursive text-center mb-4">Verification</h2>
+          <p className="text-center font-mono text-black mb-6">
+            {verificationMethod === 'mobile' ? 'You will get OTP via SMS' : 'You will get OTP via Email'}
+          </p>
+          <div className="flex justify-center mb-4">
+            <input
+              type="text"
+              className="border border-gray-300 rounded-lg px-4 py-2 w-full text-center"
+              maxLength={4}
+              placeholder="• • • •"
+            />
+          </div>
+          <button className="w-full bg-custom-red text-white py-2 rounded-lg hover:bg-pink-600 transition duration-300">
+            Verify
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Verification;
